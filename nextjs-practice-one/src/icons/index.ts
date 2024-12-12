5e5bb0e7b7271b8d14da6e038ec7863f50ebb9ed
HuongLe <huong.le@asnet.com.vn>
2024-12-12 16:07:40 +0700

    Remove redundant icons and folder ui

--- nextjs-practice-one/src/icons/index.ts
+++ nextjs-practice-one/src/icons/index.ts
@@ -1,7 +1,3 @@
-export * from './BoltIcon';
-export * from './BugAntIcon';
-export * from './CubeIcon';
-export * from './ChevronRightIcon';
 export * from './UserIcon';
 export * from './OutlineStartIcon';
 export * from './StartIcon';
