f823789018dd37ef8dc36620e954d56d128cb638
HuongLe <huong.le@asnet.com.vn>
2024-12-16 17:44:32 +0700

    #29 Update main layout and onboarding layout

--- nextjs-practice-one/src/layouts/MainLayout.tsx
+++ nextjs-practice-one/src/layouts/MainLayout.tsx
@@ -6,7 +6,9 @@ import { Footer } from '@/sections';
 export const MainLayout = ({ children }: { children: ReactNode }) => (
   <main>
     <section
-      className={`h-[calc(100vh-(88px+60px))] lg:h-[calc(100vh-(88px+128px))]`}
+      // 88px is the height of the footer
+      // 60px is the height of the header mobile and 128px for desktop
+      className={`min-h-[calc(100vh-(88px+60px))] lg:min-h-[calc(100vh-(88px+128px))]`}
     >
       {children}
     </section>
--- nextjs-practice-one/src/layouts/OnboardingLayout.tsx
+++ nextjs-practice-one/src/layouts/OnboardingLayout.tsx
@@ -2,7 +2,10 @@ import { ReactNode } from 'react';
 
 export const OnboardingLayout = ({ children }: { children: ReactNode }) => (
   <main>
-    <section className={`h-[calc(100vh-60px)] lg:h-[calc(100vh-128px)]`}>
+    <section
+      // 60px is the height of the header mobile and 128px for desktop
+      className={`min-h-[calc(100vh-60px)] lg:min-h-[calc(100vh-128px)] relative"`}
+    >
       {children}
     </section>
   </main>
