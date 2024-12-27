896506f79ba90c9cf9b48d586d978e2a7a3a57f8
HuongLe <huong.le@asnet.com.vn>
2024-12-27 18:28:44 +0700

    Create function get booking appointments

--- nextjs-practice-one/src/actions/booking.ts
+++ nextjs-practice-one/src/actions/booking.ts
@@ -2,12 +2,13 @@
 import { httpClient } from '@/services';
 
 // Constants
-import { API_ENDPOINT, QUERY_FILTER_URL } from '@/constants';
+import { API_ENDPOINT, QUERY_FILTER_URL, QUERY_URL } from '@/constants';
 
 // Types
 import {
   BookingAppointmentPayload,
   BookingAppointmentPayloadResponse,
+  BookingSlotResponse,
   TimeSlotResponse,
 } from '@/types';
 
@@ -18,6 +19,24 @@ export const getTimeSlot = async () => {
   try {
     const data = await httpClient.get<TimeSlotResponse>(
       `${API_ENDPOINT.TIME_SLOT}?${QUERY_FILTER_URL.SORT_BY_TIME}`,
+    );
+
+    return {
+      data: data.data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      data: [],
+      error: getErrorMessage(error),
+    };
+  }
+};
+
+export const getBookingAppointment = async (userId: number) => {
+  try {
+    const data = await httpClient.get<BookingSlotResponse>(
+      `${API_ENDPOINT.BOOKING_SLOT}${QUERY_URL.APPOINTMENT_BY_USER_ID(userId)}`,
     );
 
     return {
