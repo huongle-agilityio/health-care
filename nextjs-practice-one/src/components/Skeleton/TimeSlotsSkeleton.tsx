983ab31a0001bd9cd074723bd608145c4d0edc73
HuongLe <huong.le@asnet.com.vn>
2024-12-30 17:26:04 +0700

    Update skeleton for time slots

--- nextjs-practice-one/src/components/Skeleton/TimeSlotsSkeleton.tsx
+++ nextjs-practice-one/src/components/Skeleton/TimeSlotsSkeleton.tsx
@@ -5,13 +5,23 @@ import { cn } from '@/utils';
 
 export const TimeSlotsSkeleton = () => (
   <div className="flex flex-col gap-6">
-    <Skeleton className={cn('rounded-xl', 'w-[110px] h-[20px]')}>
+    <Skeleton className={cn('rounded-xl', 'w-full 2xl:w-[110px] h-[20px]')}>
       <div className="bg-default-300" />
     </Skeleton>
-    <Skeleton className={cn('rounded-xl', 'w-[110px] h-[20px]')}>
+    <Skeleton
+      className={cn(
+        'rounded-xl',
+        'w-full md:w-[110px] h-[20px] md:hidden xl:block',
+      )}
+    >
       <div className="bg-default-300" />
     </Skeleton>
-    <Skeleton className={cn('rounded-xl', 'w-[110px] h-[20px]')}>
+    <Skeleton
+      className={cn(
+        'rounded-xl',
+        'w-full md:w-[110px] h-[20px] md:hidden xl:block',
+      )}
+    >
       <div className="bg-default-300" />
     </Skeleton>
   </div>
