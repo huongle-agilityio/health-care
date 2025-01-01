66f4ffa4b58cad07cc49d74887c89a5b498a2d20
HuongLe <huong.le@asnet.com.vn>
2025-01-01 21:23:54 +0700

    Remove redundant vercel config

--- nextjs-practice-one/next.config.mjs
+++ nextjs-practice-one/next.config.mjs
@@ -9,19 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'max-age=0, s-maxage=86400, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
