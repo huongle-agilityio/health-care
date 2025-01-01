603c0a027df4e4cb5b4d45aef92be16b8d9a8e06
HuongLe <huong.le@asnet.com.vn>
2025-01-01 19:21:54 +0700

    Update header of next config

--- nextjs-practice-one/next.config.mjs
+++ nextjs-practice-one/next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=0, s-maxage=86400, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
