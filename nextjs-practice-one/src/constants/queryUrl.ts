8676e9aa4f7608ed7e6ff72d87a5fd7de871151c
Huong Nguyen Lan Le <huong.le@asnet.com.vn>
2025-02-05 09:44:16 +0700

    #135 Implement api history booking for all patients

--- nextjs-practice-one/src/constants/queryUrl.ts
+++ nextjs-practice-one/src/constants/queryUrl.ts
@@ -20,6 +20,7 @@ export const QUERY_FILTER_URL = {
   BY_SPECIALTY: (specialty: string) =>
     specialty && `&filters[specialty][name][$eq]=${specialty}`,
   SORT_BY_TIME: 'sort[time]=asc',
+  SORT_BY_DATE: 'sort[date]=desc',
 };
 
 export const QUERY_URL = {
@@ -41,4 +42,6 @@ export const QUERY_URL = {
     `?filters[date][$eq]=${date}&populate[timeSlot][fields][0]=time&filters[doctor][documentId][$eq]=${doctorId}`,
   APPOINTMENT_BY_USER_ID: (id: string) =>
     `?filters[user][$eq]=${id}&populate[doctor][fields][0]=*&populate[timeSlot][fields][0]=time&sort[date]=asc&sort[timeSlot][time]=asc`,
+  APPOINTMENT_HISTORY: (page = CURRENT_PAGE, pageSize = 10) =>
+    `?&populate[doctor][fields][0]=*&populate[doctor][populate][specialty][fields][0]=name&populate[user][fields][0]=name&populate[timeSlot][fields][0]=time&sort[timeSlot][time]=asc&${QUERY_FILTER_URL.SORT_BY_DATE}${QUERY_FILTER_URL.PAGINATION(page, pageSize)}`,
 };
--- nextjs-practice-one/src/types/booking.ts
+++ nextjs-practice-one/src/types/booking.ts
@@ -1,4 +1,23 @@
+import { User } from './user';
+import { Doctor } from './doctor';
+import { TimeSlot } from './timeSlot';
 import { ApiPaginationResponse } from './api';
+
+export interface BookingSlot {
+  date: string;
+  doctor?: Doctor;
+  user?: User;
+  timeSlot?: TimeSlot;
+}
+
+export interface BookingHistory {
+  date: string;
+  bookings: BookingSlot[];
+}
+
+export interface BookingSlotsByDate {
+  [date: string]: BookingSlot[];
+}
 
 export interface BookingAppointmentPayload {
   data: { date: string; timeSlot: string; doctor: string; user: string };
@@ -6,3 +25,5 @@ export interface BookingAppointmentPayload {
 
 export type BookingAppointmentPayloadResponse =
   ApiPaginationResponse<BookingAppointmentPayload>;
+export type BookingSlotResponse = ApiPaginationResponse<BookingSlot[]>;
+export type BookingHistoryResponse = ApiPaginationResponse<BookingHistory[]>;
